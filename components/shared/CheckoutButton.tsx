1b20bd6bb559571463c6d76c30b4871f2133186f
vamsi <32238284+vkrish1707@users.noreply.github.com>
2024-06-03 16:25:32 -0400

    Update event details and add WhatsApp contact button

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -16,13 +16,19 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
     <div className="flex items-center gap-3">
       {hasEventFinished ? (
         <p className="p-2 text-red-400">Sorry, tickets are no longer available.</p>
-      ): (
+      ) : (
         <>
           <SignedOut>
             <Button asChild className="button rounded-full" size="lg">
               <Link href="/sign-in">
                 Get Tickets
               </Link>
+            </Button>
+
+            <Button asChild className="button rounded-full bg-green-500 text-white" size="lg">
+              <a href="https://wa.me/+14379976561?text=Hello,%20I%20am%20interested%20in%20booking%20a%20reservation%20for%20the%20India%20vs%20Pakistan%20T20%20match%20screening.%20Please%20provide%20me%20with%20the%20details.%20Thank%20you!" target="_blank" rel="noopener noreferrer">
+                Contact on WhatsApp
+              </a>
             </Button>
           </SignedOut>
 
@@ -32,7 +38,7 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
         </>
       )}
     </div>
-  )
+  );
 }
 
 export default CheckoutButton
